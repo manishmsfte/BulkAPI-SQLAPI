da71707cac327e3c6ea5dc988f6a10afe2766f92
Manish Sharma <manishmsfte@outlook.com>
2021-02-16 11:55:48 +0530

    added code

---
+++ BulkAPI/Item.cs
@@ -0,0 +1,9 @@
+﻿namespace BulkAPI
+{
+    public class Item
+    {
+        public string id {get;set;}
+        public string name { get; set; }
+        public string CartID { get; set; }
+    }
+}
---
+++ BulkAPI/Program.cs
@@ -0,0 +1,51 @@
+﻿
+using Microsoft.Azure.Cosmos;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkAPI
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            String connectionString = "AccountEndpoint=https://wmt-graph.documents.azure.com:443/;AccountKey=4j0dgD7zpsigEl1gGjmpaJMrZ4EGA9VwBe1zHEvIww4XTxo970e31euj90xSQ3kZyKmCAQbpWlqXtMRPMbkcXA==;";
+            CosmosClientOptions options = new CosmosClientOptions() { AllowBulkExecution = true , ConnectionMode=ConnectionMode.Direct, ConsistencyLevel=ConsistencyLevel.Eventual};
+            CosmosClient cosmosClient = new CosmosClient(connectionString, options);
+            Container container = cosmosClient.GetContainer("RetailDemo", "WebsiteData1");
+
+            // Assuming your have your data available to be inserted or read
+            List<Task> concurrentTasks = null; DateTime start=DateTime.Now;
+            for (int iteration = 0; iteration < 3; iteration++)
+            {
+                concurrentTasks= new List<Task>();
+                 start = DateTime.Now;
+                foreach (Item itemToInsert in ReadYourData())
+                {
+                    concurrentTasks.Add(container.CreateItemAsync(itemToInsert, new PartitionKey(itemToInsert.CartID),new ItemRequestOptions() { EnableContentResponseOnWrite=false }));
+                }
+
+                await Task.WhenAll(concurrentTasks);
+                
+            }
+            System.Console.WriteLine("Record inserted=" + concurrentTasks.Count + " in total time= " + DateTime.Now.Subtract(start).TotalMilliseconds + " ms");
+            System.Console.ReadLine();
+
+        }
+
+        private static IEnumerable<Item> ReadYourData()
+        {
+            List<Item> lstItems = new List<Item>();
+            for(int j=0;j<10;j++)
+            for (int i=0;i<1000;i++)
+            {
+                    lstItems.Add(new Item() { id = Guid.NewGuid().ToString(), name = "MNt" + i, CartID = "pk" + j });
+            }
+            return lstItems;
+        }
+    }
+}
